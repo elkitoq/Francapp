089c5970d8debcb0c22bd5b273a723250b79d3a2
elkitoq <elkitoq@gmail.com>
2021-04-20 22:31:52 -0300

    state workorders

--- src/Components/Workorders.js
+++ src/Components/Workorders.js
@@ -1,41 +1,37 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Col, NavLink, Label, List } from 'reactstrap'
+import { Row, Col, Button, ButtonGroup, Label, List } from 'reactstrap'
 import { CardWorks } from './CardWorks.js'
 import '../assets/css/card-text.css'
 import LocalBase from 'localbase'
 
-
 let db = new LocalBase('db')
-
-function GetWorks() {
-    const [state, setState] = useState(1)
-    const [works, setWorks] = useState([])
-
-    useEffect(() => {
-        db.collection('workorder').orderBy('date').limit(7).get().then(work => {
-            setWorks(work.filter(work => work.state === state))
-        })
-    }, [])
-    return works
-}
 
 export const Workorders = () => {
 
-    const workList = GetWorks();
+    const [works, setWorks] = useState([])
+    const [state, setEstate] = useState(1)
+
+    useEffect(() => {
+        db.collection('workorder').orderBy('date').limit(7).get().then(work => {
+            setWorks(work.filter(work => work === state))
+        })
+    }, [])
 
     return (
 
         <Row className="content-workorder">
             <Col xs="8" className="col-content-card">
                 <Row className="row-nav">
-                    <NavLink clasName="link" href="#">Terminados</NavLink>
-                    <NavLink clasName="link" href="#">En espera</NavLink>
-                    <NavLink clasName="link" href="#">Entregados</NavLink>
+                    <ButtonGroup size="lg">
+                        <Button onClick={setEstate(1)} >En Espera</Button>
+                        <Button onClick={setEstate(2)}>Terminados</Button>
+                        <Button onClick={setEstate(3)}>Entregados</Button>
+                    </ButtonGroup>
                 </Row>
 
                 <Col xs="11" id="no-scroll1" style={{ maxHeight: '70%', overflowY: 'auto', overflowX: 'hidden' }}>
                     <List clasName="list-card" >
-                        {workList.map(datos => <CardWorks key={datos.id} props={datos} />)}
+                        {works.map(datos => <CardWorks key={datos.id} props={datos} />)}
                     </List>
                 </Col>
 
